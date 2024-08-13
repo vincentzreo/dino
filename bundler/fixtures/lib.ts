f446b04c5c8055cadc22a5f672cebf91b4cb4c17
zhouzhangqi <1978892854@qq.com>
2024-08-13 22:43:58 +0800

    feature: extract dune bundler code to bundler

---
+++ bundler/fixtures/lib.ts
@@ -0,0 +1,10 @@
+async function execute(name: string): Promise<string> {
+    console.log('Executing lib');
+    return `Hello ${name}`;
+}
+
+function not_used() {
+    console.log('This function is not used');
+}
+
+export { execute, not_used };
---
+++ bundler/src/lib.rs
@@ -0,0 +1,27 @@
+mod bundle;
+mod modules;
+mod transpilers;
+use anyhow::Result;
+
+pub use bundle::run_bundle;
+
+pub trait ModuleLoader {
+    fn load(&self, specifier: &str) -> Result<ModuleSource>;
+    fn resolve(&self, base: Option<&str>, specifier: &str) -> Result<ModulePath>;
+}
+
+pub type ModulePath = String;
+pub type ModuleSource = String;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+
+    #[test]
+    fn bundle_ts_should_work() -> Result<()> {
+        let ret = run_bundle("fixtures/main.ts", &Default::default())?;
+        assert_eq!(ret, "async function execute(name){console.log(\"Executing lib\");return`Hello ${name}`;}async function main(){console.log(\"Executing main\");console.log(await execute(\"world\"));}export{main as default};");
+        Ok(())
+    }
+}
