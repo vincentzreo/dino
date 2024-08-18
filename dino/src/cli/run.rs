f78a39fb7011c5508a6bb1bf811a83979e051277
zhouzhangqi <1978892854@qq.com>
2024-08-18 22:28:23 +0800

    feature: use IntoJs/FromJs for rust/js interop

--- dino/src/cli/run.rs
+++ dino/src/cli/run.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 use clap::Parser;
 
-use crate::{build_project, CmdExecutor, JsWorker};
+use crate::{build_project, CmdExecutor, JsWorker, Request};
 
 #[derive(Debug, Parser)]
 
@@ -14,7 +14,13 @@ impl CmdExecutor for RunOpts {
         let content = fs::read_to_string(&filename)?;
         let worker = JsWorker::try_new(&content)?;
         // TODO: normally this should run axum server
-        worker.run("await handlers.hello()")?;
+        let req = Request::builder()
+            .method("GET".to_string())
+            .url("http://localhost:3000".to_string())
+            .headers(Default::default())
+            .build();
+        let ret = worker.run("hello", req)?;
+        println!("Response: {:?}", ret);
         Ok(())
     }
 }
--- dino/src/engine.rs
+++ dino/src/engine.rs
@@ -1,5 +1,8 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
-use rquickjs::{Context, Function, Object, Runtime};
+use rquickjs::{Context, FromJs, Function, IntoJs, Object, Promise, Runtime};
+use typed_builder::TypedBuilder;
 
 #[allow(unused)]
 pub struct JsWorker {
@@ -9,6 +12,24 @@ pub struct JsWorker {
 
 fn print(msg: String) {
     println!("{}", msg);
+}
+
+#[derive(Debug, TypedBuilder)]
+pub struct Request {
+    pub headers: HashMap<String, String>,
+    #[builder(setter(into))]
+    pub method: String,
+    #[builder(setter(into))]
+    pub url: String,
+    #[builder(default, setter(strip_option))]
+    pub body: Option<String>,
+}
+
+#[derive(Debug)]
+pub struct Response {
+    pub status: u16,
+    pub headers: HashMap<String, String>,
+    pub body: Option<String>,
 }
 
 impl JsWorker {
@@ -29,12 +50,43 @@ impl JsWorker {
         Ok(Self { rt, ctx })
     }
 
-    pub fn run(&self, script: &str) -> Result<()> {
+    pub fn run(&self, name: &str, req: Request) -> Result<Response> {
         self.ctx.with(|ctx| {
-            ctx.eval_promise(script)?.finish()?;
-            Ok::<_, anyhow::Error>(())
-        })?;
-        Ok(())
+            let global = ctx.globals();
+            let handlers: Object = global.get("handlers")?;
+            let func: Function = handlers.get(name)?;
+            let v: Promise = func.call((req,))?;
+            Ok::<_, anyhow::Error>(v.finish()?)
+        })
+    }
+}
+
+impl<'js> IntoJs<'js> for Request {
+    fn into_js(self, ctx: &rquickjs::Ctx<'js>) -> rquickjs::Result<rquickjs::Value<'js>> {
+        let obj = Object::new(ctx.clone())?;
+
+        obj.set("headers", self.headers.into_js(ctx))?;
+        obj.set("method", self.method.into_js(ctx))?;
+        obj.set("url", self.url.into_js(ctx))?;
+        obj.set("body", self.body.into_js(ctx))?;
+
+        Ok(obj.into())
+    }
+}
+
+impl<'js> FromJs<'js> for Response {
+    fn from_js(ctx: &rquickjs::Ctx<'js>, value: rquickjs::Value<'js>) -> rquickjs::Result<Self> {
+        let obj = Object::from_js(ctx, value)?;
+
+        let status: u16 = obj.get("status")?;
+        let headers: HashMap<String, String> = obj.get("headers")?;
+        let body: Option<String> = obj.get("body")?;
+
+        Ok(Response {
+            status,
+            headers,
+            body,
+        })
     }
 }
 
@@ -45,9 +97,15 @@ mod tests {
     #[test]
     fn js_worker_should_run() {
         let code = r#"
-        (function(){async function hello(){print("hello world");return"hello";}return{hello:hello};})();"#;
+        (function(){async function hello(req){return{status:200,headers:{"content-type":"application/json"},body: JSON.stringify(req)};}return{hello:hello};})();"#;
 
+        let req = Request::builder()
+            .method("GET")
+            .url("http://localhost:8080")
+            .headers(HashMap::new())
+            .build();
         let worker = JsWorker::try_new(code).unwrap();
-        worker.run("await handlers.hello()").unwrap();
+        let ret = worker.run("hello", req).unwrap();
+        assert_eq!(ret.status, 200);
     }
 }
