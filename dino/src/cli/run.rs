1acba9e7f84f325dbb860a16cc58461343e2df39
zhouzhangqi <1978892854@qq.com>
2024-08-23 01:54:27 +0800

    feature: want to notify file change, but did not work

--- dino/src/cli/run.rs
+++ dino/src/cli/run.rs
@@ -1,8 +1,9 @@
-use std::fs;
+use std::{fs, path::Path};
 
 use clap::Parser;
 use dino_server::{start_server, ProjectConfig, SwappableAppRouter, TenentRouter};
-use tracing::level_filters::LevelFilter;
+use notify::{RecursiveMode, Watcher};
+use tracing::{level_filters::LevelFilter, warn};
 use tracing_subscriber::{
     fmt::Layer, layer::SubscriberExt as _, util::SubscriberInitExt as _, Layer as _,
 };
@@ -24,11 +25,26 @@ impl CmdExecutor for RunOpts {
         let filename = build_project(".")?;
         let code = fs::read_to_string(&filename)?;
         let config = ProjectConfig::load(filename.replace(".mjs", ".yml"))?;
-        let routers = vec![TenentRouter::new(
-            "localhost",
-            SwappableAppRouter::try_new(&code, config.routes)?,
-        )];
+        let router = SwappableAppRouter::try_new(&code, config.routes)?;
+        let routers = vec![TenentRouter::new("localhost", router.clone())];
+        // TODO: can't work
+        watch_project(".", router)?;
         start_server(self.port, routers).await?;
         Ok(())
     }
+}
+
+fn watch_project(dir: &'static str, router: SwappableAppRouter) -> anyhow::Result<()> {
+    let mut watcher = notify::recommended_watcher(move |res| match res {
+        Ok(event) => {
+            warn!("event: {:?}", event);
+            let filename = build_project(dir).unwrap();
+            let config = ProjectConfig::load(filename.replace(".mjs", ".yml")).unwrap();
+            let code = fs::read_to_string(&filename).unwrap();
+            router.swap(&code, config.routes).unwrap();
+        }
+        Err(e) => warn!("watch error: {:?}", e),
+    })?;
+    watcher.watch(Path::new(dir), RecursiveMode::Recursive)?;
+    Ok(())
 }
