cb32f30caacf5870fa967db1a4fc0f42ae70a998
zhouzhangqi <1978892854@qq.com>
2024-08-25 22:42:35 +0800

    feature: support file watch to trigger router swap

--- dino/src/cli/run.rs
+++ dino/src/cli/run.rs
@@ -1,17 +1,21 @@
-use std::{fs, path::Path};
+use std::{fs, path::Path, time::Duration};
 
 use clap::Parser;
 use dino_server::{start_server, ProjectConfig, SwappableAppRouter, TenentRouter};
-use notify::{RecursiveMode, Watcher};
-use tracing::{level_filters::LevelFilter, warn};
+use notify::RecursiveMode;
+use notify_debouncer_mini::{new_debouncer, DebounceEventResult};
+use tokio::sync::mpsc::channel;
+use tokio_stream::{wrappers::ReceiverStream, StreamExt};
+use tracing::{info, level_filters::LevelFilter, warn};
 use tracing_subscriber::{
     fmt::Layer, layer::SubscriberExt as _, util::SubscriberInitExt as _, Layer as _,
 };
 
 use crate::{build_project, CmdExecutor};
 
-#[derive(Debug, Parser)]
+const MONITOR_FS_INTERVAL: Duration = Duration::from_secs(2);
 
+#[derive(Debug, Parser)]
 pub struct RunOpts {
     // port to listen
     #[arg(short, long, default_value = "3000")]
@@ -22,29 +26,57 @@ impl CmdExecutor for RunOpts {
     async fn execute(&self) -> anyhow::Result<()> {
         let layer = Layer::new().with_filter(LevelFilter::INFO);
         tracing_subscriber::registry().with(layer).init();
-        let filename = build_project(".")?;
-        let code = fs::read_to_string(&filename)?;
-        let config = ProjectConfig::load(filename.replace(".mjs", ".yml"))?;
+
+        let (code, config) = get_code_and_config()?;
         let router = SwappableAppRouter::try_new(&code, config.routes)?;
         let routers = vec![TenentRouter::new("localhost", router.clone())];
-        // TODO: can't work
-        watch_project(".", router)?;
+
+        tokio::spawn(async_watch(".", router));
         start_server(self.port, routers).await?;
         Ok(())
     }
 }
 
-fn watch_project(dir: &'static str, router: SwappableAppRouter) -> anyhow::Result<()> {
-    let mut watcher = notify::recommended_watcher(move |res| match res {
-        Ok(event) => {
-            warn!("event: {:?}", event);
-            let filename = build_project(dir).unwrap();
-            let config = ProjectConfig::load(filename.replace(".mjs", ".yml")).unwrap();
-            let code = fs::read_to_string(&filename).unwrap();
-            router.swap(&code, config.routes).unwrap();
+fn get_code_and_config() -> anyhow::Result<(String, ProjectConfig)> {
+    let filename = build_project(".")?;
+    let code = fs::read_to_string(&filename)?;
+    let config = ProjectConfig::load(filename.replace(".mjs", ".yml"))?;
+    Ok((code, config))
+}
+
+async fn async_watch(p: impl AsRef<Path>, router: SwappableAppRouter) -> anyhow::Result<()> {
+    let (tx, rx) = channel(1);
+    let mut debouncer = new_debouncer(MONITOR_FS_INTERVAL, move |res: DebounceEventResult| {
+        if let Err(e) = tx.blocking_send(res) {
+            warn!("Failed to send debouncer event: {:?}", e);
         }
-        Err(e) => warn!("watch error: {:?}", e),
     })?;
-    watcher.watch(Path::new(dir), RecursiveMode::Recursive)?;
+    debouncer
+        .watcher()
+        .watch(p.as_ref(), RecursiveMode::Recursive)?;
+
+    let mut stream = ReceiverStream::new(rx);
+    while let Some(ret) = stream.next().await {
+        match ret {
+            Ok(events) => {
+                let mut need_swap = false;
+                // config.yml change, or any .ts or .mjs file change
+                for event in events {
+                    let path = event.path;
+                    let ext = path.extension().unwrap_or_default();
+                    if path.ends_with("config.yml") || ext == "ts" || ext == "mjs" {
+                        info!("file change: {:?}", path);
+                        need_swap = true;
+                        break;
+                    }
+                }
+                if need_swap {
+                    let (code, config) = get_code_and_config()?;
+                    router.swap(code, config.routes)?;
+                }
+            }
+            Err(e) => warn!("watch error: {:?}", e),
+        }
+    }
     Ok(())
 }
